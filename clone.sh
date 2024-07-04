728d202d4ae675c0dfb2e8698cf8c9f39ae930d8
Dominic DiGiacomo <dominic.m.digiacomo@gmail.com>
2024-07-04 05:57:31 -0400

    init

---
+++ clone.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git clone git@github.com:whymidnight/nakama-plus-common.git
+git clone git@github.com:whymidnight/nakama-plus-kit.git
+git clone git@github.com:whymidnight/nakama-cluster.git
+git clone git@github.com:whymidnight/memberlist.git
+git clone git@github.com:whymidnight/nakama-common.git
+git clone git@github.com:whymidnight/nakama.git
+git clone git@github.com:whymidnight/nakama-kit.git
+git clone git@github.com:whymidnight/nakama-plus.git
